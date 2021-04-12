d18810882e5640722a5c095ebf425708233ed2e5
fearlesspinata <vesnalay@gmail.com>
2021-04-11 23:28:56 -0700

    Create character.py

---
+++ character.py
@@ -0,0 +1,8 @@
+class character:
+    def __init__(self, playerName, characterName, alignment, exp):
+        self.playerName = playerName
+        self.characterName = characterName
+        self.alignment = alignment
+        self.exp = exp
+
+    
