34798690e57ceb7b8a96dd6cd74bf0b21ac687da
Vesna Lay <vlay@Vesnas-MacBook-Pro.local>
2021-04-14 19:54:13 -0700

    Organizing a bit to make it all more readable

--- charOptions.py
+++ charOptions.py
@@ -1,4 +1,6 @@
 raceList = ['Dwarf', 'Elf', 'Halfling', 'Human']
 race = {'1': 'Dwarf', '2': 'Elf', '3': 'Halfling', '4': 'Human'}
+charClassList = ['Bard', 'Cleric', 'Fighter', 'Rogue', 'Wizard']
 charClass = {'1': 'Bard', '2': 'Cleric', '3': 'Fighter', '4': 'Rogue', '5': 'Wizard'}
+backgroundList = ['Acolyte', 'Criminal', 'Entertainer', 'Soldier']
 background = {'1': 'Acolyte', '2': 'Criminal', '3': 'Entertainer', '4': 'Soldier'}
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 #import character
 from charOptions import race
 from charOptions import raceList
+from charOptions import charClassList
 from charOptions import charClass
+from charOptions import backgroundList
 from charOptions import background
 
 def main():
     print("Welcome to the DnD Character Creator! This project is still in development  but any feedback is appreciated.")
     
     name = input("Let's start with your character's name: ")
-    enumeratedList = enumerate(raceList, 1)
-    for count, r in enumeratedList:
+    
+    enumeratedRaceList = enumerate(raceList, 1)
+    enumeratedCharClassList = enumerate(charClassList, 1)
+    enumeratedBackgroundList = enumerate(backgroundList, 1)
+    for count, r in enumeratedRaceList:
         print(count, r)
 
-    choiceRace = input(f'What race will {name} be?: ')
-    choiceRace = race[choiceRace]
+    for count, c in enumeratedCharClassList:
+        print(count, c)
+
+    for count, b in enumeratedBackgroundList:
+        print(count, b)
+
+
+
+    
 
     print(f'So far {name} is a {choiceRace}. Is this correct?: ')
 
